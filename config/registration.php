39019f9e4b871013dd0a3b2aa9618db90bec9a72
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-11-03 14:15:25 +0400

    + include valid transitions in registration config + config reader for standard plan

--- config/registration.php
+++ config/registration.php
@@ -20,26 +20,37 @@ return [
             'steps' => [
                 StandardSteps::PERSONAL_INFO->value => [
                     'order' => 1,
-                    'can_rollback' => false,
                     'validation' => [
                         'name' => 'required|string',
                         'email' => 'required|email',
                         'phone' => 'required|string',
+                    ],
+                    'destinations' => [
+                        StandardSteps::DOCUMENT_UPLOAD->value,
+                        StandardSteps::COMPLETION->value,
                     ]
                 ],
                 StandardSteps::DOCUMENT_UPLOAD->value => [
                     'order' => 2,
-                    'can_rollback' => true,
                     'external_system' => true,
+                    'destinations' => [
+                        StandardSteps::VERIFICATION->value,
+                    ]
                 ],
                 StandardSteps::VERIFICATION->value => [
                     'order' => 3,
-                    'can_rollback' => true,
                     'external_system' => true,
+                    'destinations' => [
+                        StandardSteps::PERSONAL_INFO->value,
+                        StandardSteps::COMPLETION->value,
+                    ]
                 ],
                 StandardSteps::COMPLETION->value => [
-                    'can_rollback' => true,
                     'order' => 4,
+                    'external_system' => false,
+
+                    // if empty it's an end of story
+                    'destinations' => []
                 ],
             ]
         ],
@@ -56,16 +67,25 @@ return [
             'steps' => [
                 SimplifiedSteps::QUICK_INFO->value => [
                     'order' => 1,
+                    'external_system' => false,
                     'validation' => [
                         'email' => 'required|email',
+                    ],
+                    'destinations' => [
+                        SimplifiedSteps::VERIFICATION->value,
                     ]
                 ],
                 SimplifiedSteps::VERIFICATION->value => [
                     'order' => 2,
                     'external_system' => true,
+                    'destinations' => [
+                        SimplifiedSteps::COMPLETION->value,
+                    ]
                 ],
                 SimplifiedSteps::COMPLETION->value => [
                     'order' => 3,
+                    'external_system' => false,
+                    'destinations' => [] // end
                 ]
             ]
         ]
