507b0030243afec46c6c653575a3465c67853dfc
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-11-03 18:07:46 +0400

    + add dedicated serializer for fetch plans feature -+ serializing in the feature

--- app/Services/Registrar/Features/V1/FetchPlansFeature.php
+++ app/Services/Registrar/Features/V1/FetchPlansFeature.php
@@ -4,7 +4,10 @@ namespace App\Services\Registrar\Features\V1;
 
 use App\Domains\Http\Jobs\SendJsonDataResponseJob;
 use App\Domains\Registration\Jobs\FetchPlansJob;
+use App\Services\Registrar\Http\Resources\RegistrationPlanResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
 
 readonly final class FetchPlansFeature
 {
@@ -19,6 +22,18 @@ readonly final class FetchPlansFeature
     {
         $plans = $this->fetchPlansJob->handle();
 
-        return $this->responseJob->handle($plans);
+        return $this->respond($plans);
+    }
+
+    private function respond(Collection $plans): Response
+    {
+        return $this->responseJob->handle(
+            $this->serialize($plans)
+        );
+    }
+
+    private function serialize(Collection $plans): AnonymousResourceCollection
+    {
+        return RegistrationPlanResource::collection($plans);
     }
 }
