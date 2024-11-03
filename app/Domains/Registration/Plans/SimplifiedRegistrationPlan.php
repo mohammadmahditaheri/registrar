ddac236a59752ce3ede3574be193b5f43d409df0
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-11-03 18:04:54 +0400

    + make registration context singleton based - rm redundancy and DRY plan

--- app/Domains/Registration/Plans/SimplifiedRegistrationPlan.php
+++ app/Domains/Registration/Plans/SimplifiedRegistrationPlan.php
@@ -8,22 +8,8 @@ use App\Domains\Registration\States\Simplified\SimplifiedVerificationState;
 
 class SimplifiedRegistrationPlan implements RegistrationPlanInterface
 {
-    public function getStates(): array
-    {
-        return [
-            'quick_info' => QuickInfoState::class,
-            'simplified_verification' => SimplifiedVerificationState::class,
-            'completion' => SimpleCompletionState::class,
-        ];
-    }
-
     public function getInitialState(): string
     {
         // TODO: Implement getInitialState() method.
-    }
-
-    public function getName(): string
-    {
-        // TODO: Implement getName() method.
     }
 }
