0936916bd1ddc237888b973dbccd4d5fb3ee24d9
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-11-03 14:29:10 +0400

    + conform verification and personal info states to new interface + use implementor trait (InteractsWith...)

--- app/Domains/Registration/States/Standard/VerificationState.php
+++ app/Domains/Registration/States/Standard/VerificationState.php
@@ -2,16 +2,32 @@
 
 namespace App\Domains\Registration\States\Standard;
 
+use App\Domains\Registration\Composables\InteractsWithStateConfig;
 use App\Domains\Registration\Composables\ReferencesToStateContext;
 use App\Domains\Registration\RegistrationStateInterface;
 use App\Foundation\Enums\Steps\StandardStepsEnum;
 
 class VerificationState implements RegistrationStateInterface
 {
-    use ReferencesToStateContext;
+    use ReferencesToStateContext,
+        InteractsWithStateConfig;
 
     public function getIdentifier(): string
     {
         return StandardStepsEnum::VERIFICATION->value;
+    }
+
+    /**
+     * @return array{
+     *     order: int,
+     *     external_system: bool,
+     *     destinations: bool
+     * }
+     */
+    public function getConfig(): array
+    {
+        return $this->getStepConfig(
+            step: StandardStepsEnum::VERIFICATION->value,
+        );
     }
 }
