0615bdd0f0df2d70fbc4f823af4c3392a80f121d
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-11-03 14:34:09 +0400

    + standard steps enum id to class name converter

--- app/Foundation/Enums/Steps/StandardStepsEnum.php
+++ app/Foundation/Enums/Steps/StandardStepsEnum.php
@@ -2,10 +2,29 @@
 
 namespace App\Foundation\Enums\Steps;
 
+use App\Domains\Registration\RegistrationStateInterface;
+use App\Domains\Registration\States\Standard\CompletionState;
+use App\Domains\Registration\States\Standard\DocumentUploadState;
+use App\Domains\Registration\States\Standard\PersonalInfoState;
+use App\Domains\Registration\States\Standard\VerificationState;
+
 enum StandardStepsEnum: string
 {
     case PERSONAL_INFO = 'personal_info';
     case DOCUMENT_UPLOAD = 'document_upload';
     case VERIFICATION = 'verification';
     case COMPLETION = 'completion';
+
+    public static function getStateClass(
+        string $stateIdentifier
+    ): false|string
+    {
+        return match ($stateIdentifier) {
+            self::PERSONAL_INFO->value => PersonalInfoState::class,
+            self::DOCUMENT_UPLOAD->value => DocumentUploadState::class,
+            self::VERIFICATION->value => VerificationState::class,
+            self::COMPLETION->value => CompletionState::class,
+            default => false
+        };
+    }
 }
