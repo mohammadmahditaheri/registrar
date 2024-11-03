9d4b7df13ae03dd6ec691340a09b2c4dc53eba95
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-11-03 05:07:28 +0400

    + empty controllers for interacting with plans and registrations

---
+++ app/Services/Registrar/Http/Controllers/V1/ProceedToNextStepController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Registrar\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+
+final class ProceedToNextStepController extends Controller
+{
+    public function __invoke(): Response
+    {
+        // TODO implement
+    }
+}
